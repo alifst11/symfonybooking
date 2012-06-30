6c696bcf17db8aa7c0c4d7fbeb0dab1e15c72c70
tino.zorotovic@gmail.com <tino.zorotovic@gmail.com@49631574-5049-7c60-c263-53e228992a71>
2012-06-30 11:34:20 +0000

    Maps added, partials organization

--- trunk/apps/frontend/modules/public/templates/IndexSuccess.php
+++ trunk/apps/frontend/modules/public/templates/IndexSuccess.php
@@ -28,12 +28,6 @@
   <?php include_partial('public/apartment_list', array('apartments' => $apartments)) ?>
 </div>
 
-
-
-<!-- <div id="map_home" style="width: 600px; height: 300px"></div> -->
-
-<?php // use_javascript('/frontend_dev.php/djs/global/g_map_single.js') ?>
-
 <script type="text/javascript">
       $(document).ready(function() {
 
---
+++ trunk/apps/frontend/templates/_apartment_map.php
@@ -0,0 +1,32 @@
+
+<div id="map_home" style="width:<?php echo($width) ?>px; height:<?php echo($height) ?>px"></div>
+
+<script type="text/javascript">
+
+	function show_map() {
+	  
+		var map = new google.maps.Map(document.getElementById('map_home'),{
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			center: new google.maps.LatLng(<?php echo($apartment->getGLat()) ?>, <?php echo($apartment->getGLon()) ?>),
+			zoom: 10
+		});
+
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(<?php echo($apartment->getGLat()) ?>, <?php echo($apartment->getGLon()) ?>),
+			map: map,
+			title: '<?php echo($apartment->getName()) ?>',
+		});
+
+		var listener = google.maps.event.addListener(map2, "idle", function() { 
+			if (map.getZoom() > 16) map.setZoom(16); 
+			google.maps.event.removeListener(listener); 
+		});
+
+	}
+
+	google.maps.event.addDomListener(window, 'load', show_map);
+
+</script>
+
+
+<?php // use_javascript('/frontend_dev.php/djs/global/g_map_single.js') ?>
